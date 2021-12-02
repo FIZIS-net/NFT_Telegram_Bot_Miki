0ccb5f2dc465d35976b2393701206018d9929b4f
FIZIS <pshift4180@gmail.com>
2021-12-02 17:27:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TOKEN    = '2097103115:AAGpvNr9g2rbDhqEIiJSTiOdezjq_rBgKHU' # bot token from @BotFather
-DISTOKEN = 'OTA2NzMxMjI1ODQ1MjE5NDIx.YYc5Qg.WpQ9E34oqnV-B0m2oA6Xfgi7ZC4'
+TOKEN    = '' # bot token from @BotFather
+DISTOKEN = ''
 
 HEADERS  = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
 
